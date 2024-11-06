b92fd81fb29a208bb26711be268ea7a0aff86b12
Absar Ahmed <68219741+absarahmed59@users.noreply.github.com>
2024-11-06 23:25:41 +0500

    Tables

---
+++ song/Inserting Into SalGrade.sql
@@ -0,0 +1,14 @@
+INSERT INTO SalaryGrade(grade, lowSalary, highSalary)
+VALUES(1, 700, 1200);
+
+INSERT INTO SalaryGrade(grade, lowSalary, highSalary)
+VALUES(2, 1201, 1400);
+
+INSERT INTO SalaryGrade(grade, lowSalary, highSalary)
+VALUES(3, 1401, 2000);
+
+INSERT INTO SalaryGrade(grade, lowSalary, highSalary)
+VALUES(4, 2001, 3000);
+
+INSERT INTO SalaryGrade(grade, lowSalary, highSalary)
+VALUES(5, 3001, 9999);
---
+++ song/Intersting Into Employee.sql
@@ -0,0 +1,41 @@
+INSERT INTO Employee(employeeNo, employeeName, job, manager, hireDate, salary, commission, departmentNo)
+VALUES(7839,'King', 'President', NULL, '1981-11-17', 5000, NULL, 10);
+
+INSERT INTO Employee(employeeNo, employeeName, job, manager, hireDate, salary, commission, departmentNo)
+VALUES(7698,'Blake', 'Manager', 7839, '1981-05-01',2850, NULL, 30);
+
+INSERT INTO Employee(employeeNo, employeeName, job, manager, hireDate, salary, commission, departmentNo)
+VALUES(7782,'Clark', 'Manager', 7839, '1981-06-09',2450, NULL, 10);
+
+INSERT INTO Employee(employeeNo, employeeName, job, manager, hireDate, salary, commission, departmentNo)
+VALUES(7566,'Jones', 'Manager', 7839, '1981-04-02',2975, NULL, 20);
+
+INSERT INTO Employee(employeeNo, employeeName, job, manager, hireDate, salary, commission, departmentNo)
+VALUES(7654,'Martin', 'Salesman', 7698, '1981-09-28',1250, 1400, 30);
+
+INSERT INTO Employee(employeeNo, employeeName, job, manager, hireDate, salary, commission, departmentNo)
+VALUES(7499,'Allen', 'Salesman', 7698, '1981-02-20',1600, 300, 30);
+
+INSERT INTO Employee(employeeNo, employeeName, job, manager, hireDate, salary, commission, departmentNo)
+VALUES(7844,'Turner', 'Salesman', 7698, '1981-09-08',1500, 0, 30);
+
+INSERT INTO Employee(employeeNo, employeeName, job, manager, hireDate, salary, commission, departmentNo)
+VALUES(7900,'James', 'Clerk', 7698, '1981-12-03', 950, NULL, 30);
+
+INSERT INTO Employee(employeeNo, employeeName, job, manager, hireDate, salary, commission, departmentNo)
+VALUES(7521,'Ward', 'Salesman', 7698, '1981-02-22', 1250, 500, 30);
+
+INSERT INTO Employee(employeeNo, employeeName, job, manager, hireDate, salary, commission, departmentNo)
+VALUES(7902,'Ford', 'Analyst', 7566, '1981-12-03', 3000, NULL, 20);
+
+INSERT INTO Employee(employeeNo, employeeName, job, manager, hireDate, salary, commission, departmentNo)
+VALUES(7369,'Smith', 'Clerk', 7902, '1980-12-17', 800, NULL, 20);
+
+INSERT INTO Employee(employeeNo, employeeName, job, manager, hireDate, salary, commission, departmentNo)
+VALUES(7788,'Scott', 'Analyst', 7566, '1982-12-09', 3000, NULL, 20);
+
+INSERT INTO Employee(employeeNo, employeeName, job, manager, hireDate, salary, commission, departmentNo)
+VALUES(7876,'Adams', 'Clerk', 7788, '1983-01-12', 1100, NULL, 20);
+
+INSERT INTO Employee(employeeNo, employeeName, job, manager, hireDate, salary, commission, departmentNo)
+VALUES(7934,'Miller', 'Clerk', 7782, '1982-01-23', 1300, NULL, 10);
